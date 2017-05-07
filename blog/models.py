bd52e3e22061e84f012f5435ae945ac07c036784
Bill <s8508235@yahoo.com.tw>
2017-05-07 13:36:54 +0800

    add user system

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class User(models.Model):
+    username=models.CharField(max_length=200)
+    email=models.EmailField(max_length=254)
+    #password=models.
+
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from forms import SignUpForm
 # Create your views here.
+
+def home(request):
+	return render(request, 'blog/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'blog/signup.html', {'form': form})
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request,'blog/post_list.html',{'posts':posts})
+
