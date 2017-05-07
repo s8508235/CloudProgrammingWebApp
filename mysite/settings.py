0deef03d3a99718c44db5b3380c543f8ac5babaa
Bill <s8508235@yahoo.com.tw>
2017-05-07 23:23:54 +0800

    12

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^5(qdw&8xsb5s^$#*i*!s@*5l+!weburx8-^_(21)fit#xi=x3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-env.5j3dyxuv5u.us-east-1.elasticbeanstalk.com','127.0.0.1']
 
