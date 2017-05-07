3a6585f44c4b84b1cb46ccc07680c263ffb21596
Bill <s8508235@yahoo.com.tw>
2017-05-07 13:43:43 +0800

    add posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
 	url(r'^$',views.home,name='home'),
-    url(r'^posts/$',views.post_list,name='post_list'),
+    url(r'^accounts/profile/$',views.post_list,name='post_list'),
     url(r'^signin/$',auth_views.login, {'template_name': 'blog/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
